5e3bcab9f75b110b651f29918d11cb4b1d901df3
Colin Tindle <ampersand@underscore.space>
2018-04-26 11:43:53 +0100

    Full coverage on Shape

--- app/tests/Shape.spec.js
+++ app/tests/Shape.spec.js
@@ -14,4 +14,46 @@ describe('Shape', () => {
     expect(shape.position.x).toBeLessThanOrEqual(9)
     expect(shape.grid).toBeInstanceOf(Array)
   })
+
+  test('move method - positive move', () => {
+    const shape = new Shape()
+    const startX = shape.position.x
+    const startY = shape.position.y
+    shape.move(1,1)
+    expect(shape.position.x).toBe(startX+1)
+    expect(shape.position.y).toBe(startY+1)
+  })
+
+  test('move method - negative move', () => {
+    const shape = new Shape()
+    const startX = shape.position.x
+    const startY = shape.position.y
+    shape.move(-1,-1)
+    expect(shape.position.x).toBe(startX-1)
+    expect(shape.position.y).toBe(startY-1)
+  })
+
+  test('rotate CCW', () => {
+    const testShape = shapes[0]
+    const shape = new Shape()
+    shape.grid = testShape.grid
+    shape.type = testShape.type
+    shape.position = {x: testShape.start, y: 0}
+
+    const expected = [[1,0],[1,0],[1,1]]
+    shape.rotateCCW()
+    expect(shape.grid).toEqual(expected)
+  })
+
+  test('rotate CW', () => {
+    const testShape = shapes[0]
+    const shape = new Shape()
+    shape.grid = testShape.grid
+    shape.type = testShape.type
+    shape.position = {x: testShape.start, y: 0}
+
+    const expected = [[1, 1], [0, 1], [0, 1]]
+    shape.rotateCW()
+    expect(shape.grid).toEqual(expected)
+  })
 })
