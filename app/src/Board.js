e93c5a154cf2e518ee398d5e2ba64e4d1357bc23
Colin Tindle <ampersand@underscore.space>
2018-05-29 13:25:54 +0100

    added Board class

---
+++ app/src/Board.js
@@ -0,0 +1,25 @@
+export default class Board {
+  constructor(w, h) {
+    this.grid = new Array(h).fill(new Array(w).fill('x'))
+    this.size = {w,h}
+  }
+
+  copy() {
+    return this.grid.map(row => row.slice())
+  }
+
+  clearFullRows() {
+    let lines = 0
+    let rows = []
+    for (let row of this.grid) {
+      if (row.includes('x')) {
+        rows.push(row.slice())
+      } else {
+        rows.unshift(new Array(this.size.w).fill('x'))
+        lines++
+      }
+    }
+    this.grid = rows
+    return lines
+  }
+}
