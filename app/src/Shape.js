886b77ce194681e04325e308943f587b887110bd
Colin Tindle <ampersand@underscore.space>
2018-05-29 09:14:27 +0100

    created Shape class per new design code stripped out for v2

--- app/src/Shape.js
+++ app/src/Shape.js
@@ -1,77 +1,44 @@
-import shapes from './shapes'
+import shapes from 'shapes';
 
 export default class Shape {
-  constructor(boardSize, board) {
-    this.atBottom = false
-    this.resetNext = false
+  constructor() {
+    const shape = shapes[Math.floor(Math.random() * shapes.length)]
 
-    this.boardSize = boardSize
-    this.board = board
-
-    const {type, grid, start, rotateFix} = this.randomShape()
-    this.type = type
-    this.grid = grid
-    this.position = {x: start, y: 2}
-    this.rotateFix = rotateFix
-    this.rotateFixPos = 0
-
-    if (!this.positionOk(this.grid, this.position)) {
-      this.gameover = true
-    }
+    this.grid = shape.grid
+    this.pos = {x: shape.start, y:2}
+    this.rotateFix = shape.rotateFix
+    this.rotateFixNdx = 0
   }
 
-  randomShape() {
-    const min = 0
-    const max = shapes.length - 1
-    const rnd = Math.floor(Math.random() * (max - min + 1) + min)
-
-    return shapes[rnd]
+  move(x) {
+    this.pos.x += x
   }
 
-  size() {
-    return {
-      w: this.grid[0].length,
-      h: this.grid.length
-    }
+  drop() {
+    this.pos.y++
   }
 
-  plummet() {
-    while (!this.resetNext) {
-      this.move(0,1)
-    }
-  }
+  rotateCW() {
+    const grid = this.grid
+    const newGrid = [...Array(grid[0].length)].map(e => Array(grid.length))
 
-  move(x, y) {
-    if (this.resetNext) { return }
-    if (y<0) { y=0 }
-
-    const newPosition = {
-      x: this.position.x + x,
-      y: this.position.y + y
-    }
-
-    if (this.positionOk(this.grid, newPosition)) {
-      this.position = Object.assign({}, newPosition)
-      this.atBottom = false
-      this.resetNext = false
-    } else if (y == 1) {
-      if (this.atBottom) {
-        this.resetNext = true
-        return
+    for (let row in grid) {
+      for (let col in grid[row]) {
+        newGrid[col][grid.length-row-1] = grid[row][col]
       }
-      this.atBottom = true
-      return
-    }
+  }
 
-    this.checkBottom()
+    this.grid = newGrid
+
+    this.rotateFixNdx++
+    if (this.rotateFixNdx >= this.rotateFix.length) {this.rotateFixNdx = 0}
+
+    this.pos.x += this.rotateFix[this.rotateFixNdx].x
+    this.pos.y += this.rotateFix[this.rotateFixNdx].y
   }
 
   rotateCCW() {
-    if (this.resetNext) { return }
-    if (this.type == 'o') { return }
-
-    
-    const grid = this.grid.slice()
+    const grid = this.grid
     const newGrid = [...Array(grid[0].length)].map(e => Array(grid.length))
 
     for (let row in grid) {
@@ -80,96 +47,12 @@ export default class Shape {
       }
     }
 
-    const newPosition = {
-      x: this.position.x - this.rotateFix[this.rotateFixPos].x,
-      y: this.position.y - this.rotateFix[this.rotateFixPos].y
-    }
+    this.grid = newGrid
 
-    let newRotateFixPos =  this.rotateFixPos-1
-    if (newRotateFixPos == -1) {newRotateFixPos = this.rotateFix.length-1}
+    this.pos.x -= this.rotateFix[this.rotateFixNdx].x
+    this.pos.y -= this.rotateFix[this.rotateFixNdx].y
 
-    if (this.positionOk(newGrid, newPosition)) {
-      this.grid = newGrid
-      this.position = Object.assign({}, newPosition)
-      this.rotateFixPos = newRotateFixPos
-    }
-
-    this.checkBottom()
-  }
-
-  rotateCW() {
-    if (this.resetNext) { return }
-    if (this.type == 'o') { return }
-
-    const grid = this.grid.slice()
-    const newGrid = [...Array(grid[0].length)].map(e => Array(grid.length))
-
-    for (let row in grid) {
-      for (let col in grid[row]) {
-        newGrid[col][grid.length-row-1] = grid[row][col]
-      }
-    }
-
-    let newRotateFixPos = this.rotateFixPos+1
-    if (newRotateFixPos == this.rotateFix.length) {newRotateFixPos = 0}
-    
-    const newPosition = {
-      x: this.position.x + this.rotateFix[newRotateFixPos].x,
-      y: this.position.y + this.rotateFix[newRotateFixPos].y
-    }
-
-    if (this.positionOk(newGrid, newPosition)) {
-      this.grid = newGrid
-      this.position = Object.assign({}, newPosition)
-      this.rotateFixPos = newRotateFixPos
-    }
-
-    this.checkBottom()
-  }
-
-  positionOk(grid, position) {
-    if ((position.x < 0) || (position.x+grid[0].length) > this.boardSize.w) {
-      return false
-    }
-
-    if (position.y+grid.length > this.boardSize.h) {
-      return false
-    }
-
-    for (let row in grid) {
-      for (let col in grid[row]) {
-        if (grid[row][col] === 0) { continue }
-        if (this.board[Number(row)+position.y][Number(col)+position.x] != 'x') {
-          return false
-        }
-      }
-    }
-
-    return true
-  }
-
-  checkBottom() {
-    const size = this.size()
-    let position = this.position
-    const boardSize = this.boardSize
-
-    if (position.y+size.h == boardSize.h) {
-      this.atBottom = true
-    } else {
-      this.atBottom = false
-      this.resetNext = false
-    }
-  }
-
-  addTo(rows) {
-    for (let row in this.grid){
-      for (let col in this.grid[row]) {
-        if (this.grid[row][col] == 1) {
-          rows[Number(row)+this.position.y][Number(col)+this.position.x] = this.type
-        }
-      }
-    }
-
-    return rows
+    this.rotateFixNdx--
+    if (this.rotateFixNdx < 0) {this.rotateFixNdx = this.rotateFix.length-1}
   }
 }
--- app/tests/Shape.spec.js
+++ app/tests/Shape.spec.js
@@ -1,152 +1,57 @@
 jest.mock('../src/shapes')
-import Shape from '../src/Shape'
-import Board from '../src/Board'
+import Shape from "../src/Shape";
 
-describe('Shape', () => {
+describe("Shape()", () => {
   let shape
-  let board
-  
+
   beforeEach(() => {
-    board = new Board(10,19)
-    shape = board.shape
+    shape = new Shape();
   })
 
-  test('move method - positive move', () => {
-    const startX = shape.position.x
-    const startY = shape.position.y
-    shape.move(1,1)
-    expect(shape.position.x).toBe(startX+1)
-    expect(shape.position.y).toBe(startY+1)
+  test("Starting point", () => {
+    expect(shape.pos).toEqual({x:3,y:2})
   })
 
-  test('move method - negative move', () => {
-    const startX = shape.position.x
-    const startY = shape.position.y
-    shape.move(-1,-1)
-    expect(shape.position.x).toBe(startX-1)
-    expect(shape.position.y).toBe(startY)
+  test("move(1)", () => {
+    shape.move(1)
+    expect(shape.pos).toEqual({x:4,y:2})
   })
 
-  test('rotate CCW', () => {
-    const expected = [[1, 0], [1, 1], [1, 0]]
-    shape.rotateCCW()
-    expect(shape.grid).toEqual(expected)
+  test("move(-1)",  () => {
+    shape.move(-1)
+    expect(shape.pos).toEqual({x:2,y:2})
   })
 
-  test('rotate CW', () => {
-    const expected = [[0, 1], [1, 1], [0, 1]]
+  test("drop()", () => {
+    shape.drop()
+    expect(shape.pos).toEqual({x:3,y:3})
+  })
+
+  test("rotateCW()", () => {
     shape.rotateCW()
-    expect(shape.grid).toEqual(expected)
+    expect(shape.grid).toEqual([[0, 1], [1, 1], [0, 1]])
+    expect(shape.pos).toEqual({x:3,y:1})
   })
 
-  test('you can\'t rotate square', () => {
-    const expected = [[1, 1, 1], [0, 1, 0]]
-    shape.type = 'o'  // trickery
-
-    shape.rotateCCW()
-    expect(shape.grid).toEqual(expected)
-
+  test("rotateCW() all the way round", () => {
     shape.rotateCW()
-    expect(shape.grid).toEqual(expected)
-  })
-
-  test('shape can\'t be rotated when on left', () => {
-    const expected = [[1, 0], [1, 1], [1, 0]]
-    // setup
-    const board = new Board(10, 19)
-    const shape = board.shape
-    shape.rotateCCW()
-    shape.move(-4,3)
-
-    // check we're on left edge
-    expect(shape.position).toEqual({x:0,y:4})
-    expect(shape.grid).toEqual(expected)
-
-    // tests
-    shape.rotateCCW()
-    expect(shape.position).toEqual({x:0,y:4})
-    expect(shape.grid).toEqual(expected)
     shape.rotateCW()
-    expect(shape.position).toEqual({x:0,y:4})
-    expect(shape.grid).toEqual(expected)
-  })
-
-  test('shape can\'t be rotated when on right', () => {
-      // setup
-      const expected = [[0, 1], [1, 1], [0, 1]]
-      const board = new Board(10, 19)
-      const shape = board.shape
-      shape.rotateCW()
-      shape.move(5,3)
-  
-      // check we're on left edge
-      expect(shape.position).toEqual({x:8,y:4})
-      expect(shape.grid).toEqual(expected)
-      // tests
-      shape.rotateCCW()
-      expect(shape.position).toEqual({x:8,y:4})
-      expect(shape.grid).toEqual(expected)
-      shape.rotateCW()
-      expect(shape.position).toEqual({x:8,y:4})
-      expect(shape.grid).toEqual(expected)
-  })
-
-  test('shape can be rotated when on right - 1 (regression test)', () => {
-    // setup
-    const expected = [[0, 1], [1, 1], [0, 1]]
-    const board = new Board(10, 19)
-    const shape = board.shape
     shape.rotateCW()
-    shape.move(4,3)
+    shape.rotateCW()
+    expect(shape.grid).toEqual([[1,1,1],[0,1,0]])
+  })
 
-    // check we're on left edge
-    expect(shape.position).toEqual({x:7,y:4})
-    expect(shape.grid).toEqual(expected)
-    // tests
+  test("rotateCCW()", () => {
     shape.rotateCCW()
-    expect(shape.position).toEqual({x:7,y:5})
-    expect(shape.grid).toEqual([[1, 1, 1], [0, 1, 0]])
-    shape.rotateCW()
-    expect(shape.position).toEqual({x:7,y:4})
-    expect(shape.grid).toEqual(expected)
+    expect(shape.grid).toEqual([[1, 0], [1, 1],[1, 0]])
+    expect(shape.pos).toEqual({x:4,y:1})
   })
 
-
-  test("this.atBottom = true when shape touches bottom of board", () => {
-    const board = new Board(10, 19)
-    const shape = board.shape
-
-    shape.move(0, 15)
-
-    expect(shape.position).toEqual({x:3,y:17})
-    expect(shape.atBottom).toBeTruthy()
-  })
-
-  test("shape can't rotate when center of piece is on bottom", () => {
-    const board = new Board(10, 19)
-    const shape = board.shape
-
+  test("rotateCCW() all the way round", () => {
     shape.rotateCCW()
     shape.rotateCCW()
-    shape.move(0, 16)
-    shape.rotateCW()
-
-    expect(shape.position).toEqual({x:3,y:17})
+    shape.rotateCCW()
+    shape.rotateCCW()
+    expect(shape.grid).toEqual([[1,1,1],[0,1,0]])
   })
-
-  // test("shape can't overlap another", () => {
-  //   const board = new Board(10, 19)
-  //   const shape = board.shape
-
-  //   shape.move(0, 15)
-  //   board.update()
-  //   board.update()
-  //   board.newShape()
-  //   // shape.move(0, 13)
-  //   // board.update()
-
-  //   console.log(board.board)
-
-  //   expect(shape.position).toEqual({x:3,y:15})
-  // })
 })
